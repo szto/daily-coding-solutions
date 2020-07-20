47c57b2d69f7c9c07cd6090bc8f655dff8f9a69c
szto <sztolabs@gmail.com>
2020-07-20 09:37:26 +0900

    11-day: solved

---
+++ solutions/11-day.py
@@ -0,0 +1,31 @@
+"""
+
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Twitter.
+
+Implement an autocomplete system. That is, given a query string s and a set of all possible query strings, return all strings in the set that have s as a prefix.
+
+For example, given the query string de and the set of strings [dog, deer, deal], return [deer, deal].
+
+Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries.
+"""
+
+def search_string(array, string):
+
+    bunch_query = dict()
+    for word in array:
+        for num, w in enumerate(word):
+            if not bunch_query.get(word[:num+1], None):
+                bunch_query[word[:num+1]] = [word]
+            else:
+                bunch_query[word[:num+1]].append(word)
+
+    return bunch_query[string]
+
+
+assert search_string(["dog", "deer", "deal"], "de") == ['deer', 'deal']
+
+
+
+
