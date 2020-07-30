ec5ef20705ea1df4c7650f086cacaf70b139114a
김순 <szto@gimsun-ui-MacBookPro.local>
2020-07-30 21:27:47 +0900

    18-day: not solved yet

--- solutions/18-day.py
+++ solutions/18-day.py
@@ -12,9 +12,11 @@ For example, given array = [10, 5, 2, 7, 8, 7] and k = 3, we should get: [10, 7,
 7 = max(5, 2, 7)
 8 = max(2, 7, 8)
 8 = max(7, 8, 7)
-Do this in O(n) time and O(k) space. You can modify the input array in-place and you do not need to store the results. You can simply print them out as you compute them.
+Do this in O(n) time and O(k) space. You can modify the input array in-place and you do not need to store the results.
+You can simply print them out as you compute them.
 """
 from collections import deque
+
 
 def get_sub_max(l, k):
     d = deque()
@@ -31,6 +33,7 @@ def get_sub_max(l, k):
     m.append(max(d))
 
     return m
+
 
 assert get_sub_max([10, 5, 2, 7, 8, 7], 3) == [10, 7, 8, 8]
 assert get_sub_max([5, 2, 1], 2) == [5, 2]
